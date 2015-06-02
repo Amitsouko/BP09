190e49b3fdcc936f67ffb075ca3db1180d19b83f
Amitsouko <benoit.marcanthony@gmail.com>
2015-06-02 15:19:23 +0200

    add date in user entity

--- site/src/Bp/ProfileBundle/Entity/User.php
+++ site/src/Bp/ProfileBundle/Entity/User.php
@@ -20,6 +20,41 @@ class User extends BaseUser
     protected $id;
 
     /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $postalCode;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $country;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $newsLetter;
+
+    /**
      * @ORM\OneToMany(targetEntity="Bp\ProductBundle\Entity\Contract", mappedBy="user")
      **/
     private $contracts;
@@ -104,5 +139,166 @@ class User extends BaseUser
     public function getContracts()
     {
         return $this->contracts;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set lastname
+     *
+     * @param string $lastname
+     * @return User
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * Get lastname
+     *
+     * @return string 
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     * @return User
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set postalCode
+     *
+     * @param string $postalCode
+     * @return User
+     */
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    /**
+     * Get postalCode
+     *
+     * @return string 
+     */
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     * @return User
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     * @return User
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string 
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * Set newsLetter
+     *
+     * @param boolean $newsLetter
+     * @return User
+     */
+    public function setNewsLetter($newsLetter)
+    {
+        $this->newsLetter = $newsLetter;
+
+        return $this;
+    }
+
+    /**
+     * Get newsLetter
+     *
+     * @return boolean 
+     */
+    public function getNewsLetter()
+    {
+        return $this->newsLetter;
     }
 }
