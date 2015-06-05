b75b5e41653dc9f3946857b30fb90df08afee4f5
Amitsouko <benoit.marcanthony@gmail.com>
2015-06-05 19:43:27 +0200

    add cart gestion in api

--- site/src/Bp/CartBundle/Controller/ApiController.php
+++ site/src/Bp/CartBundle/Controller/ApiController.php
@@ -42,6 +42,110 @@ class ApiController extends Controller
     }
 
     /**
+     * @Route("/cart/clear")
+     * @Template()
+     * @Method("POST")
+     */
+    public function clearCartAction(Request $request)
+    {
+        $this->checkAjax($request);   
+        $em = $this->getDoctrine()->getEntityManager();
+        $cart = $this->container->get("cart");
+        $cart->clear();
+        $cart = $cart->getCart();
+        $response = new Response(json_encode(
+                array(  
+                        "status" =>"success", 
+                        "data" => array("cart" => $cart)
+                    )
+                ));
+
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+    /**
+     * @Route("/cart/set-quantity")
+     * @Template()
+     * @Method("GET")
+     */
+    public function addToCartAction(Request $request)
+    {
+        $this->checkAjax($request);   
+        $type = $request->get("type");
+        $id = $request->get("id");
+        $quantity = $request->get("quantity");
+
+        if(!$type || !$id || !$quantity) return $this->returnError("La requête doit comporter 'type', 'id' et 'quantity'");
+
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $cart = $this->container->get("cart");
+
+        try{
+            $item = $this->getItem($type,$id);
+        }catch(\Exception $e) {
+            return $this->returnError($e->getMessage());
+        }
+        
+        $cart->addObject($item, $quantity);
+
+
+        $cart = $cart->getCart();
+        $response = new Response(json_encode(
+                array(  
+                        "status" =>"success", 
+                        "data" => array("cart" => $cart)
+                    )
+                ));
+
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+
+    /**
+     * @Route("/cart/remove")
+     * @Template()
+     * @Method("GET")
+     */
+    public function removeToCartAction(Request $request)
+    {
+        $this->checkAjax($request);   
+        $type = $request->get("type");
+        $id = $request->get("id");
+
+
+        if(!$type || !$id ) return $this->returnError("La requête doit comporter 'type', 'id'.");
+
+
+        $em = $this->getDoctrine()->getEntityManager();
+        $cart = $this->container->get("cart");
+
+        try{
+            $item = $this->getItem($type,$id);
+        }catch(\Exception $e) {
+            return $this->returnError($e->getMessage());
+        }
+
+         $cart->removeObject($item);
+
+
+
+        $cart = $cart->getCart();
+        $response = new Response(json_encode(
+                array(  
+                        "status" =>"success", 
+                        "data" => array("cart" => $cart)
+                    )
+                ));
+
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+
+    /**
      * @Route("/products")
      * @Template()
      * @Method("GET")
@@ -53,6 +157,9 @@ class ApiController extends Controller
         $this->checkAjax($request);  
         $em = $this->getDoctrine()->getEntityManager();
         $products = $em->getRepository("BpProductBundle:Product")->findPagination($offset,$limit);
+
+        if(count($products) ==0) return $this->returnError("0 produits renvoyé");
+
         $productArray = array();
         $photo = new Photo();
 
@@ -91,6 +198,8 @@ class ApiController extends Controller
         $filter = ($request->get("filter")) ? $request->get("filter") : "type_medium";
         $em = $this->getDoctrine()->getEntityManager();
         $product = $em->getRepository("BpProductBundle:Product")->findOneById($id);
+
+        if(!$product) return $this->returnError("Pas de produit pour cet id");
 
         $galery = array();
         foreach($product->getPhotos() as $f)
@@ -134,23 +243,26 @@ class ApiController extends Controller
         $offset = $request->get("offset");
         $this->checkAjax($request);  
         $em = $this->getDoctrine()->getEntityManager();
-        $products = $em->getRepository("BpProductBundle:Product")->findPagination($offset,$limit);
-        $productArray = array();
+        $packs = $em->getRepository("BpProductBundle:Product")->findPagination($offset,$limit);
+
+        if(count($packs) ==0) return $this->returnError("0 pack renvoyé");
+
+        $packArray = array();
         $photo = new Photo();
 
-        foreach($products as $p)
+        foreach($packs as $p)
         {
             if($p["path"])
             {
                 $p["path"]  =   $photo->getUploadDir() . $p["path"];
             }
-            $productArray[] = $p;
+            $packArray[] = $p;
         }
 
         $response = new Response(json_encode(
                 array(  
                         "status" =>"success", 
-                        "data" => array ("packs" => $productArray, "offset" => $offset, "limit" =>$limit)
+                        "data" => array ("packs" => $packArray, "offset" => $offset, "limit" =>$limit)
                     )
                 ));
 
@@ -173,5 +285,40 @@ class ApiController extends Controller
     private function checkAjax($request)
     {
         if( $this->container->getParameter("kernel.environment") != "dev" && !$request->isXmlHttpRequest()) throw $this->createNotFoundException('Le produit n\'existe pas');
+    }
+
+    private function returnError($name)
+    {
+        $response = new Response(json_encode(
+                array(  
+                        "status" =>"error", 
+                        "error" => $name,
+                        "data" => null
+                    )
+                ));
+
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+    private function getItem($type,$id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        switch ($type) {
+            case 'Pack':
+                $item = $em->getRepository("BpProductBundle:Pack")->findOneActiveById($id);
+                break;
+            
+            case 'Product':
+                $item = $em->getRepository("BpProductBundle:Product")->findOneActiveById($id);
+                break;
+            case "CustomPack":
+                $item = $em->getRepository("BpProductBundle:CustomPack")->findOneActiveById($id);
+            default:
+                throw new \Exception("Désolé, ce type n'existe pas. Choisissez entre 'Pack', 'Product', 'CustomPack'.");
+                break;
+        }
+        return ($item) ? $item : false;
     }
 }
