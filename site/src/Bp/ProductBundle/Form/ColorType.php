d6dd7731ec361fa9b18dfdbc11c35057e1c10469
Amitsouko <benoit.marcanthony@gmail.com>
2015-06-24 10:28:20 +0200

    add crud

---
+++ site/src/Bp/ProductBundle/Form/ColorType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Bp\ProductBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ColorType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('color')
+            ->add('products')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Bp\ProductBundle\Entity\Color'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'bp_productbundle_color';
+    }
+}
